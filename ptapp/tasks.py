3f29c16da96f1661279e49ec716e7b21bbda241a
Scott McKittrick <samckittrick@users.noreply.github.com>
2020-03-28 14:57:52 -0700

    Demonstrated that you can access environment variables from the celery worker running the task

--- ptapp/tasks.py
+++ ptapp/tasks.py
@@ -4,13 +4,13 @@
 
 from . import celery
 import time
+from flask import current_app
 
 @celery.task(bind=True)
 def long_task(self):
     print("Long Task")
     self.update_state(state='STARTED', meta={'value': "hello"})
     time.sleep(20)
-    raise NotImplementedException
     self.update_state(state='PROGRESS', meta={'value': "Still Working"})
     time.sleep(20)
-    return "I Finished!"
+    return current_app.config['CELERY_RESULT_VALUE']

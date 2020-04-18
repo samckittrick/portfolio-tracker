b53557b662335cebe6f19ba2725df41ebd11871a
Scott McKittrick <samckittrick@users.noreply.github.com>
2020-04-17 22:47:23 -0700

    Added account types to model

--- ptapp/main/models.py
+++ ptapp/main/models.py
@@ -4,9 +4,22 @@ from django.db import models
 
 #Describes a particular account being tracked.
 class Accounts(models.Model):
+    CASH_TYPE = "cash"
+    CD_TYPE = "cd"
+    BOND_TYPE = "bond"
+    STOCK_TYPE = "stock"
+
+    typeChoices = [
+        (CASH_TYPE, "Cash Account"),
+        (CD_TYPE, "Certificate of Deposit"),
+        (BOND_TYPE, "Bond"),
+        (STOCK_TYPE, "Stock")
+    ]
+
     name = models.CharField(max_length=200)
     bank = models.CharField(max_length=200)
-
+    type = models.CharField(max_length=6, choices = typeChoices, default=CASH_TYPE)
+    
     def __str__(self):
         return "%s at %s" % (self.name, self.bank)
 
